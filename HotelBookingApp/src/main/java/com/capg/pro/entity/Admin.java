b09448a633afa650f87d869814da27f3ba21c0a7
Sankeerthi Ganni <77227436+sankeerthi94@users.noreply.github.com>
2022-02-10 16:03:17 +0530

    Update Admin.java

--- HotelBookingApp/src/main/java/com/capg/pro/entity/Admin.java
+++ HotelBookingApp/src/main/java/com/capg/pro/entity/Admin.java
@@ -1,5 +1,58 @@
 package com.capg.pro.entity;
 
+import java.util.Objects;
+
 public class Admin {
+	
+	private int admin_id;
+	private String admin_name;
+	private String password;
+	
+	public Admin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getAdmin_id() {
+		return admin_id;
+	}
+	public void setAdmin_id(int admin_id) {
+		this.admin_id = admin_id;
+	}
+	public String getAdmin_name() {
+		return admin_name;
+	}
+	public void setAdmin_name(String admin_name) {
+		this.admin_name = admin_name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(admin_id, admin_name, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Admin other = (Admin) obj;
+		return admin_id == other.admin_id && Objects.equals(admin_name, other.admin_name)
+				&& Objects.equals(password, other.password);
+	}
+	@Override
+	public String toString() {
+		return "Admin [admin_id=" + admin_id + ", admin_name=" + admin_name + ", password=" + password + "]";
+	}
+
 
 }
